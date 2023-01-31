356f8f914f322baddb3fe5024149e650fe415b40
Abhishek Singh Manhas <abhisheksmanhas@gmail.com>
2023-01-31 09:25:30 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String url = "https://api.chucknorris.io/jokes/random";
+
+        HttpRequest httpRequest = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest,HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
+    }
+}
